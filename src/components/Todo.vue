e7b6d263b6aa1ada30077f68762d6ddbf093e254
Alexander Alexandrov <aalexandrov@commetric.local>
2019-04-04 17:31:32 +0300

    Enable editing Todo

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="ui centered card">
-    <div class="content">
+    <div class="content" v-show="!isEditing">
       <div class="header">{{todo.title}}</div>
+      <div class="meta">{{todo.project}}</div>
+      <div class="extra-content">
+        <span class="right floated edit icon" v-on:click="showForm">
+          <i class="edit icon"></i>
+        </span>
+      </div>
     </div>
-    <div class="meta">{{todo.project}}</div>
-    <div class="extra-content">
-      <span class="right floated edit icon">
-        <i class="edit icon"></i>
-      </span>
+
+    <div class="content" v-show="isEditing">
+      <div class="ui form">
+        <div class="field">
+          <label>Title</label>
+          <input type="text" v-model="todo.title">
+        </div>
+        <div class="field">
+          <label>Project</label>
+          <input type="text" v-model="todo.project">
+        </div>
+        <div class="ui two attached buttons">
+          <button class="ui basic blue button" v-on:click="hideForm">Close X</button>
+        </div>
+      </div>
     </div>
     <div class="ui bottom attached green basic button" v-show="todo.done">Completed</div>
     <div class="ui bottom attached red basic button" v-show="!todo.done">Complete</div>
@@ -16,7 +32,15 @@
 
 <script type="text/javascript">
 export default {
-  props: ["todo"]
+  props: ["todo", "isEditing"],
+  methods: {
+    showForm() {
+      this.isEditing = true;
+    },
+    hideForm() {
+      this.isEditing = false;
+    }
+  }
 };
 </script>
 
