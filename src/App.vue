30745cd7dec9864410414dd542ffe16e2d69a541
Alexander Alexandrov <aalexandrov@commetric.local>
2019-04-04 17:14:02 +0300

    show all todos using todo list component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <todo-list></todo-list>
+  <todo-list v-bind:todos="todos"></todo-list>
 </template>
 
 <script>
@@ -9,6 +9,32 @@ export default {
   name: "app",
   components: {
     TodoList
+  },
+  data() {
+    return {
+      todos: [
+        {
+          title: "Todo A",
+          project: "Project A",
+          done: false
+        },
+        {
+          title: "Todo B",
+          project: "Project B",
+          done: true
+        },
+        {
+          title: "Todo C",
+          project: "Project C",
+          done: false
+        },
+        {
+          title: "Todo D",
+          project: "Project D",
+          done: false
+        }
+      ]
+    };
   }
 };
 </script>
