8b65eca4de10bd6e52ddf800c0734e51c1011824
Aleksandrov91 <a.aleksandrov91@gmail.com>
2019-04-08 23:01:27 +0300

    Create component for todo listing

---
+++ src/components/List.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="ui grid">
+    <todo
+      v-on:delete-todo="deleteTodo"
+      v-on:complete-todo="completeTodo"
+      v-for="todo in todos"
+      v-bind:todo="todo"
+      v-bind:key="todo.title"
+    ></todo>
+  </div>
+</template>
+
+<script type="text/javascript">
+import Todo from "./Todo";
+
+export default {
+  props: ["todos"],
+  components: {
+    Todo
+  },
+  methods: {
+    completeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos[todoIndex].done = true;
+      this.$notify({
+        group: "app",
+        type: "success",
+        title: "Success",
+        text: `${todo.title} todo has been completed!`
+      });
+    },
+    deleteTodo(todo) {
+      swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this todo!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+      }).then(willDelete => {
+        if (willDelete) {
+          const todoIndex = this.todos.indexOf(todo);
+          this.todos.splice(todoIndex, 1);
+
+          this.$notify({
+            group: "app",
+            type: "success",
+            title: "Success",
+            text: "Your todo has been deleted!"
+          });
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,36 +1,19 @@
 <template>
   <div>
     <h2>Pending Tasks: ({{todos.filter(todo => {return todo.done === false}).length}})</h2>
-    <div class="ui grid">
-      <todo
-        v-on:delete-todo="deleteTodo"
-        v-on:complete-todo="completeTodo"
-        v-for="todo in pendingTasks"
-        v-bind:todo="todo"
-        v-bind:key="todo.title"
-      ></todo>
-    </div>
-
+    <list v-bind:todos="pendingTasks"></list>
     <h2>Completed Tasks: ({{todos.filter(todo => {return todo.done === true}).length}})</h2>
-    <div class="ui grid">
-      <todo
-        v-on:delete-todo="deleteTodo"
-        v-on:complete-todo="completeTodo"
-        v-for="todo in completedTasks"
-        v-bind:todo="todo"
-        v-bind:key="todo.title"
-      ></todo>
-    </div>
+    <list v-bind:todos="completedTasks"></list>
   </div>
 </template>
 
 <script type="text/javascript">
-import Todo from "./Todo";
+import List from "./List";
 
 export default {
   props: ["todos"],
   components: {
-    Todo
+    List
   },
   computed: {
     pendingTasks: function() {
@@ -38,39 +21,6 @@ export default {
     },
     completedTasks: function() {
       return this.todos.filter(x => x.done === true);
-    }
-  },
-  methods: {
-    completeTodo(todo) {
-      const todoIndex = this.todos.indexOf(todo);
-      this.todos[todoIndex].done = true;
-      this.$notify({
-        group: "app",
-        type: "success",
-        title: "Success",
-        text: `${todo.title} todo has been completed!`
-      });
-    },
-    deleteTodo(todo) {
-      swal({
-        title: "Are you sure?",
-        text: "Once deleted, you will not be able to recover this todo!",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true
-      }).then(willDelete => {
-        if (willDelete) {
-          const todoIndex = this.todos.indexOf(todo);
-          this.todos.splice(todoIndex, 1);
-
-          this.$notify({
-            group: "app",
-            type: "success",
-            title: "Success",
-            text: "Your todo has been deleted!"
-          });
-        }
-      });
     }
   }
 };
